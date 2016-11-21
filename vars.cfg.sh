002a5f6f27e0a1bf94ec08e6492f66659ddb0565
Roman Sevko <k6-2@ya.ru>
2016-11-21 08:57:35 +0300

    Add files via upload
    version 0.1 with separate files

--- vars.cfg.sh
+++ vars.cfg.sh
@@ -3,7 +3,7 @@ MYHOME=/home/$MYUSER
 if [ -z ${MYSITE+z} ]; then
 	MYSITE=my.amilinux # enter name (DNS) of your system here
 fi
-OS_VER_SHOW=
+OS_VER_SHOW=""
 AUTOEXEC_FILE=".bashrc"
 MYSH=/etc/profile.d/my.sh
 #MYCERT_DIR=$MYHOME/certs
