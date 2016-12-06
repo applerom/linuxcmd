a4970caf506bba9e6889f9ea8ae4774b61ada25c
Roman Sevko <k6-2@ya.ru>
2016-12-06 13:27:22 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# check for root at first!"
-if [[ $UID != 0 ]] ; then 
-    if sudo -s ; then 
-        echo "Run only under user with root permissions! Run 'sudo -s' and repeat your command again."
-        echo "=========================================================="
-        exit 1
-    fi
-fi
-
 MYSTART=myamilinux
 
+# check for root at first!"
+if [[ $UID != 0 ]] ; then 
+    echo "Run only under root! Add sudo at the begin and repeat your command again."
+    echo "Ex.:"
+    echo 'sudo bash -c "$(wget -q -O- https://raw.githubusercontent.com/applerom/$MYSTART/master/start.sh)"'
+    echo "=========================================================="
+    exit 1
+fi
 
 # create tmp dir"
 MY_TMP_DIR=$(mktemp -d /tmp/my_script.XXX) # create_tmp_dir
@@ -37,17 +36,6 @@ echo "=========================================================="
 echo "___ start $MYSTART.sh from git"
 cd $MYSTART
 chmod +x $MYSTART.sh
-
-echo "MYSITE = $MYSITE"
-echo "all = $@"
-
-if [ "$1" ] ; then
-	echo "MYSITE = $1"
-	echo "MYSITEa = $@"
-	MYSITE= $1
-else
-	echo "MYSITE = default"
-fi
 
 ./$MYSTART.sh
 echo "=========================================================="
