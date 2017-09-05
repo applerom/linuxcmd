0db542bd1093fde395225f7304219eb30cabeaea
Roman Sevko <apple@rom.by>
2017-09-05 10:46:33 +0300

    fix MacOS padding
    fix padding issue in Mac OS

--- prompt.sh
+++ prompt.sh
@@ -7,39 +7,41 @@ myprompt()
         fi
 
         HOSTNAME=`hostname`
-        tput setaf 4
-        printf "%*s" $(($(tput cols)-24-${#HOSTNAME}-${#USER})) "" | sed "s| |_|g"
-
+        tput setaf 4 # Set foreground color Blue
+        # make flexible padding
+        PADDING=`printf "%*s" $(($(tput cols)-24-${#HOSTNAME}-${#USER})) "" | sed "s| |_|g"`
+        printf "$PADDING"
+        
         if [ $LASTOPER == "GOOD" ]; then
-                tput setaf 2
+                tput setaf 2 # Set foreground color Green
                 printf " ✓ "
         else
-                tput setaf 1
+                tput setaf 1 # Set foreground color Red
                 tput bold
                 printf " ✗ "
         fi
 
-        tput sgr0
+        tput sgr0 # Turn off all attributes
 
         if [[ $EUID == 0 ]]; then
                 printf ""
         else
-                tput setaf 1
+                tput setaf 1 # Set foreground color Red
                 printf "$USER"
-                tput setaf 7
+                tput setaf 7 # Set foreground color White
                 printf "@"
         fi
 
-        tput setaf 3
+        tput setaf 3 # Set foreground color Yellow
         printf "$HOSTNAME"
 
-        tput setaf 4
+        tput setaf 4 # Set foreground color Blue
         printf " $(date +%F)"
 
         if [ $LASTOPER == "GOOD" ]; then
-                tput setaf 2
+                tput setaf 2 # Set foreground color Green
         else
-                tput setaf 1
+                tput setaf 1 # Set foreground color Red
                 tput bold
         fi
                 printf " $(date +%T)"
